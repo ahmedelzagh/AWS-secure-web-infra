a1f1ad2c7728b70c6d2472a54501b47dc2923889
Ahmed Elzagh <ahmedelzagh50@gmail.com>
2024-10-19 18:08:58 +0300

    Add VPC module with subnets

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to deploy resources"
+  type        = list(string)
+}
